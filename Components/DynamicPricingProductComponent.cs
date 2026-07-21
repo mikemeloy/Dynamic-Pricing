ef49d890a9f7448b1da16a89e5af77432de2cbbd
Mike Meloy <mike@i7media.net>
2026-07-21 08:54:55 -0500

    initial commit

---
+++ Components/DynamicPricingProductComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Nop.Web.Areas.Admin.Models.Catalog;
+using Nop.Web.Framework.Components;
+
+namespace i7MEDIA.Plugin.Misc.Dynamic.Pricing.Components;
+
+public class DynamicPricingProductComponent : NopViewComponent
+{
+    public IViewComponentResult Invoke(string widgetZone, ProductModel additionalData)
+    {
+        return View("~/Plugins/i7MEDIA.Plugin.Misc.Dynamic.Pricing/Areas/Admin/views/_product.dynamic.price.cshtml", additionalData);
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,43 @@
+﻿using i7MEDIA.Plugin.Misc.Dynamic.Pricing.Components;
+using Nop.Services.Cms;
+using Nop.Services.Plugins;
+using Nop.Web.Framework.Infrastructure;
+
+namespace i7MEDIA.Plugin.Misc.Dyanmic.Pricing;
+
+public class Plugin() : BasePlugin, IWidgetPlugin
+{
+    public bool HideInWidgetList => throw new NotImplementedException();
+
+    public override Task InstallAsync()
+    {
+        return base.InstallAsync();
+    }
+
+    public override Task UninstallAsync()
+    {
+        return base.UninstallAsync();
+    }
+
+    public override string GetConfigurationPageUrl()
+    {
+        return base.GetConfigurationPageUrl();
+    }
+
+    public Task<IList<string>> GetWidgetZonesAsync()
+    {
+        return Task.FromResult<IList<string>>(new List<string> {
+            AdminWidgetZones.ProductDetailsBlock
+        });
+    }
+
+    public Type GetWidgetViewComponent(string widgetZone)
+    {
+        if (widgetZone == AdminWidgetZones.ProductDetailsBlock)
+        {
+            return typeof(DynamicPricingProductComponent);
+        }
+
+        return typeof(DynamicPricingProductComponent);
+    }
+}
