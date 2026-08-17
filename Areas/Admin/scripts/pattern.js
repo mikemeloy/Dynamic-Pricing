287668a8749dd414f4b021f55d908578581b39f7
Mike Meloy <mike@i7media.net>
2026-08-17 09:23:08 -0500

    add dynamic pricing to pattern products

--- Areas/Admin/scripts/pattern.js
+++ Areas/Admin/scripts/pattern.js
@@ -11,6 +11,13 @@ const
       save = document.querySelector('[data-dynamic-price-save]');
 
     save.addEventListener('click', event.onSave_Click);
+  },
+  getTableSelections = () => {
+    if (Array.isArray(window.selectedIds)) {
+      return window.selectedIds;
+    }
+
+    return [];
   }
 
 const
@@ -20,20 +27,26 @@ const
 
       try {
         const
-          formData = new FormData();
+          formData = new FormData(),
+          patternIds = getTableSelections();
 
-        formData.append('PatternIds', selectedIds);
- 
+        formData.append('PatternIds', patternIds);
+
         const
           response = await fetch(_saveUrl, {
             method: "POST",
             body: formData
           });
 
-        console.table(response);
+        if (response.status !== 200) {
+          throw new Error();
+        }
+
+        displayBarNotification?.("Success! All products associated with the selected pattern(s) have been set to dynamic pricing", 0, 1000 * 5);
 
       } catch (error) {
         console.error(error);
+        displayBarNotification?.("An error has occurred, please check logs for more information", 2, 1000 * 5);
       }
 
       currentTarget.disabled = false;
